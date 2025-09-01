42f89409e9c15736f4bd22463169eff5dcb4156d
andridge <munenengarw@gmail.com>
2025-09-01 11:03:23 +0000

    Working Updates on simulation

--- src/august_description/src/listener.cpp
+++ src/august_description/src/listener.cpp
@@ -1,12 +1,11 @@
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
 #include <string>
-#include <vector>
 #include <iostream>
 
 void jointStateCallback(const sensor_msgs::JointState::ConstPtr& msg)
 {
-  std::string output = "Received joint positions: ";
+  std::string output = "Received joint states: ";
   for (size_t i = 0; i < msg->name.size(); ++i) {
     output += msg->name[i] + "=" + std::to_string(msg->position[i]) + " ";
   }
@@ -17,7 +16,9 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "joint_listener");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("drone_joint_commands", 10, jointStateCallback);
+
+  // Subscribe to the controller's published joint states
+  ros::Subscriber sub = nh.subscribe("/joint_states", 10, jointStateCallback);
 
   ros::spin();
   return 0;
--- src/august_description/src/talker.cpp
+++ src/august_description/src/talker.cpp
@@ -1,42 +1,38 @@
 #include "ros/ros.h"
-#include "sensor_msgs/JointState.h"
-#include <vector>
-#include <string>
-#include <cmath>
+#include "std_msgs/Float64.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "joint_talker");
   ros::NodeHandle nh;
-  ros::Publisher pub = nh.advertise<sensor_msgs::JointState>("drone_joint_commands", 10);
 
-  ros::Rate loop_rate(2); // 2 Hz
-  //std::vector<std::string> joint_names = {"Revolute47", "Revolute48", "Revolute49", "Revolute50"};
-  std::vector<std::string> joint_names;
-  joint_names.push_back("Revolute 47");
-  joint_names.push_back("Revolute 48");
-  joint_names.push_back("Revolute 49");
-  joint_names.push_back("Revolute 50");
-  int count = 0;
+  // Publishers for each propeller effort controller
+  ros::Publisher pub47 = nh.advertise<std_msgs::Float64>("/august/august_controller/revolute_47_effort_controller/command", 10);
+  ros::Publisher pub48 = nh.advertise<std_msgs::Float64>("/august/august_controller/revolute_48_effort_controller/command", 10);
+  ros::Publisher pub49 = nh.advertise<std_msgs::Float64>("/august/august_controller/revolute_49_effort_controller/command", 10);
+  ros::Publisher pub50 = nh.advertise<std_msgs::Float64>("/august/august_controller/revolute_50_effort_controller/command", 10);
+
+  ros::Rate loop_rate(20); // 20 Hz update rate
+
+  std_msgs::Float64 cmd47, cmd48, cmd49, cmd50;
+  cmd47.data = 0.5;   // CW
+  cmd48.data = -0.5;  // CCW
+  cmd49.data = 0.5;   // CW
+  cmd50.data = -0.5;  // CCW
 
   while (ros::ok())
   {
-    sensor_msgs::JointState msg;
-    msg.header.stamp = ros::Time::now();
-    msg.name = joint_names;
-    double t = count * 0.5;
-   // msg.position = {sin(t), cos(t), sin(2*t), cos(2*t)};
-  msg.position.clear();
-  msg.position.push_back(sin(t));
-  msg.position.push_back(cos(t));
-  msg.position.push_back(sin(2*t));
-  msg.position.push_back(cos(2*t));
-    pub.publish(msg);
-    ROS_INFO("Publishing joint positions: [%.2f, %.2f, %.2f, %.2f]",
-      msg.position[0], msg.position[1], msg.position[2], msg.position[3]);
+    pub47.publish(cmd47);
+    pub48.publish(cmd48);
+    pub49.publish(cmd49);
+    pub50.publish(cmd50);
+
+    ROS_INFO("Publishing: [%.2f, %.2f, %.2f, %.2f]",
+             cmd47.data, cmd48.data, cmd49.data, cmd50.data);
+
     ros::spinOnce();
     loop_rate.sleep();
-    count++;
   }
+
   return 0;
 }
